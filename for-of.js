bfb56c89f60a2aec90524a3f108d47df7d0a302c
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-01 22:43:54 +0600

    19-1 what is a loop, explore for loop

---
+++ for-of.js
@@ -0,0 +1,14 @@
+const numbers = [12, 21, 32, 42, 54, 45, 64, 64];
+
+// console.log(numbers);
+
+for(const num of numbers){
+	// console.log(num);
+}
+
+const fruits =[ 'apple', 'banana', 'orange', 'pinaapple' , 'watermelon'] ;
+
+for(const fruit of fruits){
+	console.log(fruit);
+	console.log('i want to eat');
+}
---
+++ recap.js
@@ -0,0 +1,17 @@
+//const: value will not change 
+//let: value can change 
+// var: we should not use it (old model)
+
+// variable 
+const money = 200;
+
+// condition 
+if (money > 100){
+
+}
+else {
+
+}
+
+// array 
+const numbers = [12, 12, 322, 42, 5432, 5432]
