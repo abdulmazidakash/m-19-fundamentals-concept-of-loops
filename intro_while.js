90bff077a4acc07828da648b95ad6b9e358ff639
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-01 22:53:05 +0600

    19-2 introduction to while loop

---
+++ intro_while.js
@@ -0,0 +1,7 @@
+const = 5;
+
+let count = 1; //initialization
+while (count < 5){//condition
+	console.log(count);
+	count++; //increment
+}
---
+++ while.js
@@ -0,0 +1,13 @@
+const numbers = [12, 231, 34, 453, 43, 34, 34];
+for(const num of numbers){
+
+}
+console.log('something new is cooking');
+
+//------------WHILE LOOP------------
+let num = 0; //loop variable
+while (num < 5) {
+	console.log('looping', num);
+	//num = num + 1;
+	//num += 1;
+}
