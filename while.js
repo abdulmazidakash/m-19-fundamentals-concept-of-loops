67234bba0ccf7ae3721047b9c81a801eef5365ad
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-02 11:15:51 +0600

    module summary

--- while.js
+++ while.js
@@ -6,8 +6,9 @@ console.log('something new is cooking');
 
 //------------WHILE LOOP------------
 let num = 0; //loop variable
-while (num < 5) {
+while (num < 5) {//loop condition
 	console.log('looping', num);
 	//num = num + 1;
 	//num += 1;
+	num++;
 }
